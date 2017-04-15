535cb412d4dd55b1c08fc965d4a0c3d07226871c
qianpeili <qianpeili@bianfeng.com>
2017-04-15 16:56:12 +0800

    Add Agent struct and AgentMap for manager agents

--- main.go
+++ main.go
@@ -6,17 +6,36 @@ import (
 	"os"
 )
 
-func connHandler(connection net.Conn) {
-	remote_ip := connection.RemoteAddr()
-	log.Println(remote_ip, " connects.")
+var AgentMap map[string] *Agent
+
+type Agent struct {
+	conn *net.Conn
+	remoteAddr string
+}
+
+func (agent *Agent) Close() {
+	(*agent.conn).Close()
+}
+
+func NewAgent(conn *net.Conn) *Agent {
+	return &Agent{
+		conn: conn,
+		remoteAddr: (*conn).RemoteAddr().String(),
+	}
+}
+
+func agentHandler(agent *Agent) {
+	defer agent.Close()
+	log.Println(agent.remoteAddr, " connects.")
 }
 
 
 func main()  {
 
 	ln, err := net.Listen("tcp", ":8080")
-
 	defer ln.Close()
+
+	AgentMap = make(map[string] *Agent)
 
 	if err != nil {
 		log.Println("Listen failed.")
@@ -29,7 +48,10 @@ func main()  {
 			conn.Close()
 			continue
 		}
-		go connHandler(conn)
+		agent := NewAgent(&conn)
+		AgentMap[agent.remoteAddr] = agent
+
+		go agentHandler(agent)
 	}
 
 }
