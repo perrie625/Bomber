4cb2c7236859da31be9c40bf34c4caa24dbc1e89
qianpeili <qianpeili@bianfeng.com>
2017-04-15 15:35:28 +0800

    add main.go
    initial basic server

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"net"
+	"log"
+	"os"
+)
+
+func connHandler(connection net.Conn) {
+	remote_ip := connection.RemoteAddr()
+	log.Println(remote_ip, " connects.")
+}
+
+
+func main()  {
+
+	ln, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Println("Listen failed.")
+		os.Exit(1)
+	}
+	log.Println("Listen on 8080.")
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			conn.Close()
+			continue
+		}
+		go connHandler(conn)
+	}
+
+}
