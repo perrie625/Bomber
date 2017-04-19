c0005caf4fcee6449b2ab74bd9c184181dce264c
qianpeili <qianpeili@bianfeng.com>
2017-04-19 20:21:57 +0800

    Add Room struct

---
+++ room/room.go
@@ -0,0 +1,37 @@
+package room
+
+import (
+	"net"
+	"Bomber/agent"
+)
+
+var (
+	MainRoom = NewRoom()
+)
+
+type Room struct {
+	roomId string
+	maxLength int32
+	entrance chan string
+	agentMap map[string] *net.TCPConn
+}
+
+
+func (room *Room) Entry(agent agent.Agent){
+
+}
+
+func (room *Room) Exit(agent2 agent.Agent){
+
+}
+
+func (room *Room) Destroy(){
+
+}
+
+func NewRoom() *Room {
+	return &Room{}
+}
+
+
+
