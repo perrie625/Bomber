7dd8cff20810ea131f294e349c25c750b9a717d9
钱佩丽 <qianpeili@bianfeng.com>
2017-05-12 09:41:27 +0800

    Update 登陆逻辑完善

---
+++ gate/handlers/login.go
@@ -0,0 +1,29 @@
+package handlers
+
+import (
+	"Bomber/protodata"
+	"Bomber/models"
+	"github.com/golang/protobuf/proto"
+	"Bomber/network"
+	"Bomber/gate"
+	"reflect"
+)
+
+func HandleLogin(session *models.Session, rawMsg *network.RawMessage) {
+	// 一个简单的消息处理
+	msg := new(protodata.LoginRequest)
+	if err := proto.Unmarshal(rawMsg.Data, msg); err != nil {
+		return
+	}
+	resp := new(protodata.LoginResponse)
+	println(msg.Username)
+	println(msg.Password)
+	resp.Flag = protodata.FlagNum_eOk
+	resp.Desc = "haha"
+	session.Room.BroadCast(resp)
+}
+
+
+func init() {
+	gate.RegisterHandler(2, HandleLogin, reflect.TypeOf(protodata.LoginRequest{}))
+}
