27cae9bc8d51b5c7f41266dc23ac4225e129019e
钱佩丽 <qianpeili@bianfeng.com>
2017-05-16 18:22:10 +0800

    fix db init

--- db/mongo.go
+++ db/mongo.go
@@ -3,6 +3,7 @@ package db
 import (
 	"gopkg.in/mgo.v2"
 	"log"
+	"Bomber/tools"
 )
 
 var (
@@ -11,8 +12,9 @@ var (
 
 func init() {
 	var err error
-	Session, err = mgo.DialWithTimeout("mongodb://localhost:27017/admin", 10)
+	Session, err = mgo.DialWithTimeout(tools.ServerConfig.DBUrl, 10)
 	if err != nil {
+		log.Println(err.Error())
 		log.Fatal("mongodb init fatal!")
 	}
 }
